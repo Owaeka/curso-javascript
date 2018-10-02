59415250121b4993f720e4468a7be0fed328871c
Bruno <bova@gft.com>
2018-10-02 08:39:16 -0300

    Projeto JS Avançado

---
+++ javascript-avancado-i/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -0,0 +1,21 @@
+class NegociacaoController {
+
+    constructor() {
+        let $ = document.querySelector.bind(document);
+
+        this.inputData = $('#data');
+        this.inputQuantidade = $('#quantidade');
+        this.inputValor = $('#valor');
+    }
+
+    adiciona(event) {
+        event.preventDefault();
+
+        let data = new Date(...this.inputData.value.split("-")
+            .map((item, indice) => item - indice % 2)
+        );
+
+        console.log(data);
+    }
+
+}
